a7b17a90c5a5304aa7e0c9d16dff4373ae2762fd
Aileen Zhang <aileen@caltech.edu>
2023-06-10 04:31:08 -0700

    rearrange folders and add index page

---
+++ login.js
@@ -0,0 +1,48 @@
+/*
+ * Author: Aileen Zhang
+ * CS 132 Spring 2023
+ * 
+ * This is the JS file to process inputs to the login form.
+ */
+
+(function() {
+    "use strict";
+    const TICK = 10; // check time every 10 ms, so displayed time is mostly in sync
+    let timerId;
+    
+
+    function init() {
+        timerId = setInterval(() => { displayTime(); }, TICK);
+    }
+
+    /**
+     * Update current date and time
+     */
+    function displayTime() {
+        let date = new Date();
+        let secStr = date.getSeconds().toString().padStart(2,"0");
+        let minStr = date.getMinutes().toString().padStart(2,"0");
+        let hr = date.getHours();
+        let ampm;
+        let hrStr;
+        if (hr < 12) {
+            ampm = "AM";
+            if (hr == 0) { hrStr = "12"; }
+            else { hrStr = hr.toString(); }
+        }
+        else {
+            ampm = "PM";
+            if (hr == 12) { hrStr = "12"; }
+            else { hrStr = (hr-12).toString(); }
+        }
+        let day = date.getDay();
+        let month = date.toLocaleString('default', { month: 'long' });
+        let year = date.getFullYear();
+        let timeStr = `${hrStr}:${minStr}:${secStr} ${ampm}`
+        let dateStr = `${month} ${day} ${year}`;
+        qs("#time").textContent = timeStr;
+        qs("#date").textContent = dateStr;
+    }
+
+    init();
+})();
